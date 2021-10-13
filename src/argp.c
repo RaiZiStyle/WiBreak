997669dfea8de9f0a87da952e33721b959568e57
RaiZy <arthur.guyotpremel@gmail.com>
2021-10-13 16:48:59 +0200

    Handle when no argument is given

--- src/argp.c
+++ src/argp.c
@@ -3,20 +3,23 @@
 #include <argp.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
 
 const char *argp_program_version = "WiBreak 0.0.1";
 const char *argp_program_bug_address = "arthur.guyotpremel@gmail.com";
 static char doc[] = "Program used to brute force URL.";
-static char args_doc[] = "[FILENAME] [-u URL] [-w WORDLIST]";
+static char args_doc[] = "[-u URL] [-w WORDLIST]";
 static struct argp_option options[] = {
-    {"url", 'u', "URL_TO_QUERY", 0, "Compare lines instead of characters."},
-    {"wordlist", 'w', "WORDLIST", 0, "Compare words instead of characters."},
+    {"url", 'u', "URL_TO_QUERY", 0, "Compare lines instead of characters [REQUIRED]."},
+    {"wordlist", 'w', "WORDLIST", 0, "Compare words instead of characters [REQUIRED]."},
+    {"verbose", 'v', 0, OPTION_ARG_OPTIONAL, "Show more info"},
     {0}};
 
 
 // TODO: Need to handle when no argument are given.
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     struct arguments *arguments = state->input;
+    int *arg_cout = state->input;
     switch (key) {
         case 'u':
             arguments->url_to_query = arg;
@@ -25,15 +28,23 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
         case 'w':
             arguments->wordlist = arg;
             break;
-        case ARGP_KEY_ARG:
-            printf("state->arg_num : %d\n", state->arg_num);
-            if (state->arg_num < 2 ){
-                argp_usage(state);
+        case 'v':
+            arguments->verbose = 1;
+            break;
+        
+        case ARGP_KEY_END:
+            // printf("arguments->wordlist : %s, arguments->url_to_query : %s\n", arguments->wordlist, arguments->url_to_query);
+            // Code never reach even when i initialyze arguments in the main.
+            if (arguments->wordlist == NULL || arguments->url_to_query == NULL){
+                argp_failure(state, 1, 0, "required -u and -w. See --help for more information");
+                exit(ARGP_ERR_UNKNOWN);
             }
-            return 0;
+
         default:
+            // argp_usage(state);
             return ARGP_ERR_UNKNOWN;
     }
+    
     return 0;
 }
 
--- src/main.c
+++ src/main.c
@@ -4,29 +4,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 // Local lib
+#include "../header/argp.h"
 #include "../header/cJSON.h"
 #include "../header/function.h"
-#include "../header/argp.h"
 
 // TODO: https://curl.se/libcurl/c/example.html
 
-
-
-
-
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     struct arguments arguments;
 
     // arguments.mode = CHARACTER_MODE;
     // arguments.isCaseInsensitive = false;
-    arguments.wordlist = "";
-    arguments.url_to_query = "";
+    arguments.wordlist = NULL;
+    arguments.url_to_query = NULL;
 
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
-    printf("Wordlist : %s, Url : %s\n", arguments.wordlist, arguments.url_to_query);
+
+    printf("Wordlist : %s, Url : %s\n", arguments.wordlist,arguments.url_to_query);
+    // arguments.url_to_query);
+    
     // ...
 }
